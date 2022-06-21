07744e741cdb2c0ccec7c5b98bbb6819eb264241
goshin-56 <ryuzinxx@gmail.com>
2022-06-21 13:11:43 +0000

    [Add]修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,7 +2,6 @@ class Public::CommentsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @comment = current_customer.comments.new(comment_params)
-    @comment.review_id = params[:review_id]
     if @comment.save
       redirect_back(fallback_location: root_path)
     else
@@ -15,6 +14,6 @@ class Public::CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:comment_body)
+    params.require(:comment).permit(:comment_body, :review_id)
   end
 end
